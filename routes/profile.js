6b660c024080a5788d50b2f50a495ac2d595fdac
Prasanth138 <prasanthanbu777@gmail.com>
2022-12-25 11:37:22 +0530

    update

--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { User } = require("../models/user");
 
-router.get("/", async (req, res) => {
+router.post("/", async (req, res) => {
     const {email, password} = req.body;
     const user = await User.findOne({email});
 		if(user){
